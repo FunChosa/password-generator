1d78f0de98cb72385b923a93011b2b1b53ac7f0f
FunChosa <gilazieva.00@mail.ru>
2024-11-05 22:57:57 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import PasswordGenerator from "./components/PasswordGenerator/PasswordGenerator";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <PasswordGenerator />
+      </div>
+    </>
+  );
+}
+
+export default App;
